ae8ad7681e7ad95a97694be4bf1253ec393109e4
Nichole Cubbage <77998084+ncubbag@users.noreply.github.com>
2026-01-12 14:13:59 -0500

    Create extract_upstream function for DNA sequences
    Refactor DNA sequence extraction into a function.

--- 01-sequence-parsing-toolkit/01-sequence-parsing-toolkit/src/upstream_atg.py
+++ 01-sequence-parsing-toolkit/01-sequence-parsing-toolkit/src/upstream_atg.py
@@ -1,19 +1,5 @@
-#Write a program that given a DNA string, prints out the 20 characters upstream of
-#the start codon ATG
-
-#import re
-import re
-
-#user input
-i=str(input(r"Please enter a DNA sequence: "))
-
-#write a function that accepts the DNA sequence as input
-def function(i):
-   #use regular expression to match desired substring
-   x=re.search(r"[ATCG]{20}ATG", i)
-   #and print out result if present
-   if x:
-       print("20 bases upstream of the start codon ATG is: ", x.group().rstrip("ATG"))
-#call function
-function(i)    
-
+def extract_upstream(sequence: str, length: int = 20) -> str | None:
+    match = re.search(rf"[ATCG]{{{length}}}ATG", sequence.upper())
+    if match:
+        return match.group()[:-3]
+    return None
