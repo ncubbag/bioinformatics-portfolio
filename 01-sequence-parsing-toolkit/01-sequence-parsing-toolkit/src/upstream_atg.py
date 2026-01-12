8ec8f653ddc012daa55359e55bae918deb4adf0f
Nichole Cubbage <77998084+ncubbag@users.noreply.github.com>
2026-01-12 14:20:46 -0500

    Delete 01-sequence-parsing-toolkit/01-sequence-parsing-toolkit directory

--- 01-sequence-parsing-toolkit/01-sequence-parsing-toolkit/src/upstream_atg.py
+++
@@ -1,5 +0,0 @@
-def extract_upstream(sequence: str, length: int = 20) -> str | None:
-    match = re.search(rf"[ATCG]{{{length}}}ATG", sequence.upper())
-    if match:
-        return match.group()[:-3]
-    return None
